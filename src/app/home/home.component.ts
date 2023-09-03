f22510453c259ea3a91f36e63e85d2493fe5d6bd
soubky0 <soubky10@gmail.com>
2023-09-03 10:38:38 +0300

    third commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Inviter, SessionState, URI, UserAgent } from 'sip.js';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  private userAgent!: UserAgent;
+  private inviter!: Inviter;
 
+  ngOnInit() {
+    const uri = new URI('sip', 'soubky', 'sip.linphone.org');
+    const userAgentConfig = {
+      uri: uri,
+      transportOptions: {
+        wsServers: ['wss://sip.linphone.org']
+      },
+      authorizationUser: 'soubky',
+      password: 'omaromar123'
+    };
+
+    // Create a user agent instance
+    this.userAgent = new UserAgent(userAgentConfig);
+
+    // Start the user agent
+    this.userAgent.start().then(() => {
+      console.log('User agent started');
+    }).catch((error) => {
+      console.error('Failed to start user agent:', error);
+    });
+
+  }
+  
+  makeCall() {
+    // Create an outgoing invite request
+    const targetURI = new URI('sip', 'ashrafiko', 'sip.linphone.org');
+    const options = { sessionDescriptionHandlerOptions: { constraints: {} } };
+    this.inviter = new Inviter(this.userAgent, targetURI, options);
+
+    // Send the invite request
+    this.inviter.invite().then(() => {
+      console.log('Call initiated');
+    }).catch((error) => {
+      console.error('Failed to initiate call:', error);
+    });
+
+    // Handle call state changes
+    this.inviter.stateChange.addListener((newState) => {
+      if (newState === SessionState.Terminated) {
+        console.log('Call terminated');
+      }
+    });
+  }
 }
+
